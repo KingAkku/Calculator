293dab9efbfbff45abedd2cc1dd02543fc8445d2
Akhil Kumar S <145208509+KingAkku@users.noreply.github.com>
2025-01-08 21:50:23 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function appendValue(value){
+    const display = document.getElementById('display');
+    display.value += value;
+}
+
+function clearDisplay(){
+    const display = document.getElementById('display');
+    display.value = '';
+}
+
+function calculateResult(){
+    let display = document.getElementById('display');
+    try{
+        display.value = eval(display.value);
+    } catch (error){
+        display.value = 'Error';
+    }
+}
